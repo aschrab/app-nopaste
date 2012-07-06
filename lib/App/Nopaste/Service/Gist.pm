c3e1b726b72431c772afb58311137447281da226
Aaron Schrab <aaron@schrab.com>
2012-07-06 14:33:57 -0400

    Modify Gist service to work with API v3
    The version 1 API for Gists is no longer supported, so migrate to
    version 3.

--- lib/App/Nopaste/Service/Gist.pm
+++ lib/App/Nopaste/Service/Gist.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use base 'App::Nopaste::Service';
 
+use JSON;
+
 sub available         { 1 }
 sub forbid_in_default { 0 }
 
@@ -15,55 +17,39 @@ sub run {
     my ($self, %arg) = @_;
     my $ua = LWP::UserAgent->new;
 
-    my %auth = $self->_get_auth;
+    my $filename = "paste." . ( $arg{lang} || 'txt' );
+    my %payload = (
+        public => ($arg{private} ? \0 : \1),
+        files => {
+            $filename => {
+                content => $arg{text},
+            },
+        },
+    );
 
-    my $res = $ua->post(
-      'https://gist.github.com/api/v1/json/new',
-      {
-        'file_ext[gistfile1]'      => '.' . ( $arg{lang} || 'txt' ),
-        'file_contents[gistfile1]' => $arg{text},
-        %auth,
-        defined $arg{private} ?  (private => 1) : (),
-        defined $arg{desc} ? (description => $arg{desc}) : (),
-      },
+    $payload{description} = $arg{desc} if $arg{desc};
+
+    my %headers;
+    my $token = $self->_get_token;
+    $headers{Authorization} = "token $token" if $token;
+
+    my $res = $ua->post( 'https://api.github.com/gists',
+        %headers,
+        Content => JSON->new->encode(\%payload),
     );
 
     return $self->return($res);
 }
 
-sub _get_auth {
+sub _get_token {
     my ($self) = @_;
 
-    if ($ENV{GITHUB_USER} && $ENV{GITHUB_TOKEN}) {
-        my $user  = $ENV{GITHUB_USER};
-        my $token = $ENV{GITHUB_TOKEN};
-
-        return (
-            login => $user,
-            token => $token,
-        );
-    } elsif (eval "require Git; 1") {
-        my $user  = Git::config('github.user');
-        my $token = Git::config('github.token');
-
-        return unless $user and $token;
-
-        return (
-            login => $user,
-            token => $token,
-        );
-    } elsif (eval "require Config::GitLike; 1") {
-        my $gitconfig = Config::GitLike->new( confname => 'gitconfig' );
-        $gitconfig->load;
-        my $user  = $gitconfig->get( key => 'github.user' );
-        my $token = $gitconfig->get( key => 'github.token' );
-
-        return unless $user and $token;
-
-        return (
-            login => $user,
-            token => $token,
-        );
+    {
+        open my $fh, '<', "$ENV{HOME}/.gist_token";
+        if( $fh ) {
+            my $token = <$fh>;
+            return $token;
+        }
     }
 
     return;
@@ -80,7 +66,7 @@ sub return {
       return (0, "LWP Error: " . $res->content);
     }
 
-    my ($id) = $res->content =~ qr{"repo":"([0-9a-f]+)"};
+    my ($id) = $res->header('Location') =~ qr{^https://api\.github\.com/gists/(\d+)$};
 
     return (0, "Could not find paste link.") if !$id;
     return (1, "http://gist.github.com/$id");
